152ed4c6fc7ee4effb18011cc5eb946b245bbd72
Cameron Kinsella <53635597+cameronkinsella@users.noreply.github.com>
2021-07-06 01:14:43 -0230

    Add logging
    For more or less detailed logs depending on what is desired.
    Useful for development.

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import pymongo
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 STAGE = os.getenv('STAGE', 'dev')
+LOG_LEVEL = os.getenv('LOG_LEVEL', 'WARNING')
 
 mongo_client = pymongo.MongoClient("mongodb://mongodb:27017/")
 db = mongo_client["discord"]
