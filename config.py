34afafe3d7dc552bb2597ba8bb3b446fd2b73990
Cameron Kinsella <53635597+cameronkinsella@users.noreply.github.com>
2021-07-07 03:02:45 -0230

    Restricted setprefix command to guild admins only

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 import pymongo
+from discord.ext import commands
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 STAGE = os.getenv("STAGE", "dev")
@@ -7,3 +8,16 @@ LOG_LEVEL = os.getenv("LOG_LEVEL", "WARNING")
 
 mongo_client = pymongo.MongoClient("mongodb")
 db = mongo_client["discord"]
+
+
+# Common command checks
+
+
+def is_admin():
+    async def predicate(ctx):
+        if ctx.message.author.guild_permissions.administrator:
+            return True
+        await ctx.send("This is an admin-only command")
+        return False
+
+    return commands.check(predicate)
