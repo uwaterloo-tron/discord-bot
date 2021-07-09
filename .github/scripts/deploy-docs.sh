9fd1142e1ccad1426be957bcfc3529d979c6765e
Cameron Kinsella <53635597+cameronkinsella@users.noreply.github.com>
2021-07-09 04:08:56 -0230

    Change docs publish author to GitHub Actions bot

--- .github/scripts/deploy-docs.sh
+++ .github/scripts/deploy-docs.sh
@@ -9,14 +9,14 @@ function print_info() {
 }
 
 if ! git config --get user.name; then
-    git config --global user.name "${GITHUB_ACTOR}"
+    git config --global user.name "github-actions[bot]"
 fi
 
 print_info "setup with GITHUB_TOKEN"
 remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
 if ! git config --get user.email; then
-    git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+    git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
 fi
 
 git remote rm origin
