86721a48021c2904e530f7757bc9b640aded7fb7
Cameron Kinsella <53635597+cameronkinsella@users.noreply.github.com>
2021-07-18 21:49:42 -0230

    Add comments, typing, logging, and small bug fixes
    Remove database field for embed since we can get the embed from the
    message itself.

--- cogs/poll_cog.py
+++ cogs/poll_cog.py
@@ -1,14 +1,14 @@
 import logging
 import pytz
 import discord
+import dateparser
+import datetime
+import config
 from discord.ext import tasks, commands
 from discord_slash import SlashContext, cog_ext
 from discord_slash.utils.manage_commands import create_option, create_choice
 from dateutil.parser import ParserError
-import dateparser
-import datetime
-import config
-import json
+from typing import Union
 
 
 class PollCog(commands.Cog):
@@ -20,6 +20,9 @@ class PollCog(commands.Cog):
 
     @tasks.loop(seconds=30)
     async def deadline_check(self):
+        """
+        Find polls whose deadline has passed and closes them
+        """
         polls_col = config.db["polls"]
         polls_with_deadlines = polls_col.find({"deadline": {"$ne": None}})
 
@@ -27,6 +30,9 @@ class PollCog(commands.Cog):
 
         async for poll in polls_with_deadlines:
             if pytz.timezone("UTC").localize(poll["deadline"]) <= present_datetime:
+                logging.debug(
+                    f"Deadline passed for poll. Message ID: {poll['message_id']}"
+                )
                 await self.do_close(poll["message_id"], "DEADLINE PASSED")
 
     @deadline_check.before_loop
@@ -116,7 +122,21 @@ class PollCog(commands.Cog):
         choice6: str = None,
         choice7: str = None,
         choice8: str = None,
-    ):
+    ) -> None:
+        """
+        Slash command to create a new poll for the guild
+        :param ctx: SlashContext
+        :param message: The question your poll is asking
+        :param anonymous: If users should see results before the poll is closed
+        :param choice1: Choice 1
+        :param choice2: Choice 2
+        :param choice3: Choice 3
+        :param choice4: Choice 4
+        :param choice5: Choice 5
+        :param choice6: Choice 6
+        :param choice7: Choice 7
+        :param choice8: Choice 8
+        """
         if not ctx.author.guild_permissions.administrator:
             await ctx.send("This is an admin-only command")
             return
@@ -138,6 +158,7 @@ class PollCog(commands.Cog):
             choice8,
         ]
 
+        # Create embed for poll
         options_string = ""
         emote_string = ""
         for idx, option in enumerate(options):
@@ -168,30 +189,42 @@ class PollCog(commands.Cog):
         for emote in self.emojis[:option_count]:
             await message.add_reaction(emote)
 
+        # Add new poll to database
         poll = {
             "guild_id": ctx.guild.id,
             "channel_id": ctx.channel.id,
             "message_id": message.id,
             "selections": [[]] * option_count,
-            "embed": json.dumps(embed.to_dict()),
             "deadline": None,
             "anonymous": anonymous,
         }
+        logging.debug(f"Adding poll to database. Message ID: {message.id}")
         await polls_col.insert_one(poll)
 
     @commands.Cog.listener()
-    async def on_raw_reaction_add(self, payload):
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        """
+        Handle votes
+        """
+
+        # Ignore if reaction is from bot
         if payload.user_id == self.bot.user.id:
             return
 
+        # Ignore if invalid reaction emoji
         if payload.emoji.name not in self.emojis:
             return
+
         polls_col = config.db["polls"]
         poll = await polls_col.find_one(
             {"message_id": payload.message_id, "guild_id": payload.guild_id}
         )
         if poll is None:
             return
+
+        logging.debug(
+            f"Handling vote for poll. Vote: {payload.emoji.name}, Message ID: {payload.message_id}"
+        )
         channel = self.bot.get_channel(payload.channel_id)
         message = await channel.fetch_message(payload.message_id)
         await message.remove_reaction(payload.emoji.name, payload.member)
@@ -213,13 +246,15 @@ class PollCog(commands.Cog):
                     {"$pull": {f"selections.{idx}": payload.user_id}},
                 )
 
+        # Set user's vote in database
         await polls_col.update_one(
             {"message_id": payload.message_id, "guild_id": payload.guild_id},
             {"$push": {f"selections.{index}": payload.user_id}},
         )
 
+        # Update "results" field in embed if the poll is not anonymous
         if not poll["anonymous"]:
-            embed = discord.Embed.from_dict(json.loads(poll["embed"]))
+            embed = message.embeds[0]
             embed.remove_field(-1)
             poll = await polls_col.find_one(
                 {"message_id": payload.message_id, "guild_id": payload.guild_id}
@@ -239,18 +274,31 @@ class PollCog(commands.Cog):
             await message.edit(embed=embed)
 
     @commands.Cog.listener()
-    async def on_raw_message_delete(self, payload):
+    async def on_raw_message_delete(
+        self, payload: discord.RawMessageDeleteEvent
+    ) -> None:
+        """
+        Handle if poll message is deleted
+        """
         message = payload.cached_message
         polls_col = config.db["polls"]
         poll = await polls_col.find_one(
             {"message_id": message.id, "guild_id": message.guild.id}
         )
+
+        # Ignore if the message deleted wasn't a poll
         if poll is None:
             return
+
+        logging.debug(
+            f"Poll message was deleted, recreating. Message ID: {poll['message_id']}"
+        )
         await message.channel.send(
             "Please Close the poll before attempting to delete it :pray:"
         )
-        embed = discord.Embed.from_dict(json.loads(poll["embed"]))
+
+        # Resend poll message
+        embed = message.embeds[0]
         embed.remove_field(-1)
         values = " \u200B ".join(
             [f"{i} \u200B {len(j)}" for i, j in zip(self.emojis, poll["selections"])]
@@ -276,6 +324,7 @@ class PollCog(commands.Cog):
         for emote in self.emojis[: len(poll["selections"])]:
             await new_message.add_reaction(emote)
 
+        # Update database with new poll message ID
         await polls_col.update_one(
             {"message_id": message.id, "guild_id": message.guild.id},
             {"$set": {"message_id": new_message.id}},
@@ -283,14 +332,19 @@ class PollCog(commands.Cog):
 
     @commands.group()
     @config.is_admin()
-    async def poll(self, ctx):
+    async def poll(self, ctx: commands.Context) -> None:
         if ctx.invoked_subcommand is None:
             await ctx.send(
                 "Invalid: Use one of the following subcommands: deadline, close, list"
             )
 
     @poll.command()
-    async def close(self, ctx):
+    async def close(self, ctx: commands.Context) -> None:
+        """
+        Close the selected poll. Reply to the poll you want to select.
+        """
+
+        # Make sure the command is replying to a valid poll message
         if ctx.message.reference is None:
             await ctx.send("Invalid: you must reply to the poll you want to close")
             return
@@ -306,14 +360,21 @@ class PollCog(commands.Cog):
             }
         )
         if poll is None:
-            await ctx.send("This ain't a poll :clown:")
+            await ctx.send("This ain't an active poll :clown:")
             return
 
-        await ctx.message.delete()
+        logging.debug(f"Manually closing poll. Message ID: {reply}")
+        await ctx.message.delete()  # Delete the user's command message to reduce clutter
         await self.do_close(reply, "MANUALLY CLOSED")
 
     @poll.command()
-    async def list(self, ctx):
+    async def list(
+        self, ctx: Union[commands.Context, SlashContext]
+    ) -> None:  # Note: python 3.10 now allows: "commands.Context | SlashContext" notation instead of Union
+        """
+        List all currently active polls in an embed
+        """
+        logging.debug(f"Listing all active polls. Guild ID: {ctx.guild.id}")
         polls_col = config.db["polls"]
         guild_polls = polls_col.find({"guild_id": ctx.guild.id})
 
@@ -335,18 +396,24 @@ class PollCog(commands.Cog):
         await ctx.send(embed=poll_links_embed)
 
     @poll.group()
-    async def deadline(self, ctx):
+    async def deadline(self, ctx: commands.Context) -> None:
         if ctx.invoked_subcommand is None:
             await ctx.send("Use one of the following subcommands: set, remove")
 
     @deadline.command()
-    async def set(self, ctx, *, date_string):
+    async def set(self, ctx: commands.Context, *, date_string: str) -> None:
+        """
+        Set a deadline for a poll. Reply to the poll you want to select.
+        :param ctx: Context
+        :param date_string: The date of the deadline, must be parsable (https://github.com/scrapinghub/dateparser)
+        """
         if ctx.message.reference is None:
             await ctx.send(
                 "Invalid: You must reply to the poll you want to add a deadline for"
             )
             return
 
+        # Get the selected poll
         reply = ctx.message.reference.message_id
         polls_col = config.db["polls"]
         poll = await polls_col.find_one(
@@ -357,9 +424,12 @@ class PollCog(commands.Cog):
             }
         )
         if poll is None:
-            await ctx.send("This ain't a poll :clown:")
+            await ctx.send("This ain't an active poll :clown:")
             return
 
+        logging.debug(f"Setting a new deadline for a poll. Message ID {reply}")
+
+        # Parse the given deadline date
         try:
             selected_date = dateparser.parse(
                 date_string,
@@ -372,14 +442,19 @@ class PollCog(commands.Cog):
                 raise ParserError
         except (ParserError, OverflowError):
             await ctx.send(
-                'Invalid: Please enter in a duration for example: "In 5 days and 5 hours and 30 minutes" :frowning'
+                'Invalid: You must enter a valid date. Example: "In 5 days and 5 hours and 30 minutes" :frowning:'
             )
             return
+
+        # Only allow deadlines >1 minute from current time
+        # to prevent weird activity with the deadline_check
         if (selected_date - datetime.datetime.now(pytz.utc)).total_seconds() < 59:
             await ctx.send(
                 "Invalid: You cannot enter a deadline less than 1 minute long :frowning:"
             )
             return
+
+        # Set new deadline in database
         polls_col = config.db["polls"]
         await polls_col.update_one(
             {
@@ -396,7 +471,10 @@ class PollCog(commands.Cog):
                 "message_id": reply,
             }
         )
-        embed = discord.Embed.from_dict(json.loads(poll["embed"]))
+
+        # Set new deadline in poll message embed
+        poll_message = await ctx.channel.fetch_message(reply)
+        embed = poll_message.embeds[0]
         embed.remove_field(0)
         embed.insert_field_at(
             index=0,
@@ -404,11 +482,15 @@ class PollCog(commands.Cog):
             value=selected_date.strftime("%Y/%m/%d %I:%M %p %Z"),
             inline=False,
         )
-        poll_message = await ctx.channel.fetch_message(reply)
         await poll_message.edit(embed=embed)
 
     @deadline.command()
-    async def remove(self, ctx):
+    async def remove(self, ctx: commands.Context) -> None:
+        """
+        Remove the deadline for a poll. Reply to the poll you want to select.
+        :param ctx:
+        :return:
+        """
         if ctx.message.reference is None:
             await ctx.send(
                 "Invalid: You must reply to the poll you want to add a deadline for"
@@ -425,8 +507,14 @@ class PollCog(commands.Cog):
             }
         )
         if poll is None:
-            await ctx.send("This ain't a poll :clown:")
+            await ctx.send("This ain't an active poll :clown:")
             return
+
+        if poll["deadline"] is None:
+            await ctx.send("This poll already has no deadline")
+            return
+
+        logging.debug(f"Removing deadline for poll. Message ID: {reply}")
 
         await polls_col.update_one(
             {
@@ -443,7 +531,8 @@ class PollCog(commands.Cog):
                 "message_id": reply,
             }
         )
-        embed = discord.Embed.from_dict(json.loads(poll["embed"]))
+        poll_message = await ctx.channel.fetch_message(reply)
+        embed = poll_message.embeds[0]
         embed.remove_field(0)
         embed.insert_field_at(
             index=0,
@@ -451,14 +540,13 @@ class PollCog(commands.Cog):
             value="None",
             inline=False,
         )
-        poll_message = await ctx.channel.fetch_message(reply)
         await poll_message.edit(embed=embed)
 
-    async def do_close(self, message_id: int, reason: str):
+    async def do_close(self, message_id: int, reason: str) -> None:
         """
-        Close a poll with the given message id
-        :param message_id: is the message id of the poll which is wanted to be closed *must be in the database*
-        :param reason: the footer explaining why the poll was closed
+        Close the poll with the given message id
+        :param message_id: The message id of the poll to be closed *must be in the database*
+        :param reason: The footer explaining why the poll was closed
         :return: None
         """
         polls_col = config.db["polls"]
@@ -466,6 +554,7 @@ class PollCog(commands.Cog):
         channel = self.bot.get_channel(poll["channel_id"])
         poll_message = await channel.fetch_message(message_id)
 
+        # Edit poll embed to show it is closed
         embed = poll_message.embeds[0]
         embed.set_footer(text=f"****{reason}****")
         embed.set_thumbnail(
