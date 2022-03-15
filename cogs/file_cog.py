35c751d3b98658f642d3c61d81e18d2767d2c0d2
Adam Barroso <abarroso@edu.uwaterloo.ca>
2022-03-14 23:28:23 -0400

    Update comments and add typing
    This makes our codebase more readable and easier to maintain

--- cogs/file_cog.py
+++ cogs/file_cog.py
@@ -9,9 +9,12 @@ import pyheif
 import sys
 from PIL import Image
 from pdf2image import convert_from_bytes, pdfinfo_from_bytes
+from typing import Optional
 
 
-async def save_jpg_with_target_size(im, filename, target):
+async def save_jpg_with_target_size(
+    im: Image, filename: io.BytesIO, target: int
+) -> Optional[io.BytesIO]:
     # https://stackoverflow.com/questions/52259476/how-to-reduce-a-jpeg-size-to-a-desired-size/52281257#52281257
     """Save the image as JPEG with the given name at best quality that makes less than "target" bytes"""
     # Min and Max quality
@@ -46,15 +49,15 @@ class FileCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    def __init__(self, bot):
-        self.bot = bot
-
     @commands.Cog.listener()
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message) -> None:
+        """Listens for messages with specific file types and does actions depending on the file type sent"""
+        # ignore messages without attachments
         if message.author == self.bot.user or len(message.attachments) == 0:
             return
         added_pdf = False
         for attachment in message.attachments:
+            # handles case of multiple pdf attachments
             if not added_pdf and any(
                 attachment.filename.endswith(i) for i in [".pdf", ".PDF"]
             ):
@@ -62,10 +65,11 @@ class FileCog(commands.Cog):
                 await message.add_reaction(yimin_emote)
                 added_pdf = True
             elif any(attachment.filename.endswith(i) for i in [".heic", ".HEIC"]):
+                # Download PDF from Discord CDN
                 async with aiohttp.ClientSession() as session:
                     async with session.get(attachment.url) as r:
                         file = io.BytesIO(await r.read())
-
+                # read the file
                 heif_file = pyheif.read(file)
                 image = Image.frombytes(
                     heif_file.mode,
@@ -76,6 +80,8 @@ class FileCog(commands.Cog):
                     heif_file.stride,
                 )
                 with io.BytesIO() as image_binary:
+                    # Discord has a max upload size of 8MB,
+                    # so reduce image file size to 8MB.
                     image_binary = await save_jpg_with_target_size(
                         image, image_binary, 8e6
                     )
@@ -87,10 +93,12 @@ class FileCog(commands.Cog):
                             fp=image_binary, filename=attachment.filename[:-5] + ".png"
                         )
                     )
-        return
 
     @commands.Cog.listener()
-    async def on_raw_reaction_add(self, payload):
+    async def on_raw_reaction_add(
+        self, payload: discord.RawReactionActionEvent
+    ) -> None:
+        """Listens for a specific reaction on a file type and sends that file in a pre-viewable manner"""
         yimin_emote = self.bot.get_emoji(733516365243220038)
         if payload.user_id == self.bot.user.id or payload.emoji != yimin_emote:
             return
@@ -100,7 +108,7 @@ class FileCog(commands.Cog):
 
         if message.attachments == 0:
             return
-
+        # Hard-coded value that determines output image resolution
         dpi = 200  # dots per inch
         for attachment in message.attachments:
             if any(attachment.filename.endswith(i) for i in [".pdf", ".PDF"]):
@@ -113,8 +121,10 @@ class FileCog(commands.Cog):
                 info = pdfinfo_from_bytes(pdf_file, userpw=None, poppler_path=None)
 
                 max_pages = info["Pages"]
+                # Hard-coded limit for the number of pages we store in memory at once.
                 pages_in_mem = 1
                 for pg in range(1, max_pages + 1, pages_in_mem):
+                    # Convert PDF page bytes to PIL image object
                     pages = convert_from_bytes(
                         pdf_file,
                         dpi,
@@ -122,6 +132,7 @@ class FileCog(commands.Cog):
                         last_page=min(pg + pages_in_mem - 1, max_pages),
                     )
                     for idx, page in enumerate(pages):
+                        # Convert PIL image object to PNG image bytes and send
                         with io.BytesIO() as image_binary:
                             page.save(image_binary, "PNG")
                             image_binary.seek(0)
@@ -135,7 +146,6 @@ class FileCog(commands.Cog):
 
 def setup(bot):
     # Adds cog to bot from main.py
-    if (
-        config.STAGE != "dev"
-    ):  # dont add this cog if we are in dev to avoid duplication, remove for testing
+    if config.STAGE != "dev":
+        # dont add this cog if we are in dev to avoid duplication, remove for testing
         bot.add_cog(FileCog(bot))
